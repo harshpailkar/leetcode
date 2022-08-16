0cdee7ca0107b06fa81ad7a6aed5b1516e507086
Harsh Pailkar <85762743+harshpailkar@users.noreply.github.com>
2022-08-17 00:44:48 +0530

    Time: 35 ms (78.48%), Space: 10.6 MB (91.52%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        string sub = s;
-        int pos = -1;
-       for(int i = 0; i < s.length(); i++){
-           s.erase(s.begin() + i);
-           if(s.find(sub[i]) == string::npos){
-               pos = i;
-               break;
-           }
-           s = sub;
-       }
-       return pos;
+        vector<int> alpha(26);
+        
+        for(char c : s) {
+            alpha[c - 'a']++;
+        }
+        
+        int n = s.length();
+        
+        for(int i = 0; i < n; i++) {
+            if(alpha[s[i] - 'a'] == 1) return i;
+        }
+        
+        return -1;
     }
 };
